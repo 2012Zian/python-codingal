e683e46d10ab46956e4b8d9da152435fdfec599a
2012Zian <zandri.koekemoer@gmail.com>
2025-09-09 16:42:55 +0200

    Lesson 1

---
+++ Lesson 1/Print statements and comments .py
@@ -0,0 +1,11 @@
+# printing a greeting message
+print("Python on top")
+
+# printing my name
+print("My name is Zian")
+
+#printing a fun fact
+print("Im very silly")
+
+#printing addition of numbers
+print("This is the result of 3+7 ",3+7)

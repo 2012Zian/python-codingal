09d05a9fcb66b6426e384f61343e53897a1cc704
2012Zian <zandri.koekemoer@gmail.com>
2025-10-06 16:36:52 +0200

    lesson 8

---
+++ lesson 8/reverse order.py
@@ -0,0 +1,5 @@
+n = int(input("enter a number"))
+
+print("numbers from",n,"to",1)
+for i in range(n,0,-1):
+    print(i)
---
+++ lesson 8/revrse a string.py
@@ -0,0 +1,8 @@
+string = (input("Enter a word"))
+
+string2 =""
+for i in string:
+    string2 = i+string2
+
+print("original word",string)
+print("reversed word",string2)

979a68bc19bf5cadcc75e6537050b22ef7149db0
2012Zian <zandri.koekemoer@gmail.com>
2025-10-28 17:10:41 +0200

    lesson 15

---
+++ lesson 15/due amount.py
@@ -0,0 +1,7 @@
+print("due amount")
+
+paid_amount = input("enter the paid amount: ")
+due_amount = input("enter due amount: ")
+due_amount = paid_amount + due_amount
+
+print(paid_amount - due_amount)
